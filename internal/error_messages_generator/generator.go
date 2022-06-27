75bb05ab388c2a42b41e3c134321e4d43b8e3457
Anastasia Petrichuk <petrichuk.nastya@yandex.ru>
2022-06-27 14:46:52 +0300

    Добавлен генератор ошибок

--- internal/error_messages_generator/generator.go
+++ internal/error_messages_generator/generator.go
@@ -1,11 +1,19 @@
 package error_messages_generator
 
+import "math/rand"
+
 type ErrorGenerator struct{}
 
 func New() *ErrorGenerator {
 	return &ErrorGenerator{}
 }
 
+var errTexts = []string{"Какая-то ошибка(", "Проверь еще раз", "Ошибочка", "Ты пытаешься меня обмануть?", "Попробуй еще",
+	"Не удалось", "Неа", "Снова ошибка", "Опять ошибка", "И еще разок попробуй", "Перепроверь", "Что-то не то", "Неть.",
+	"Не удалось", "Давай еще раз", "Ошибся ты, а об ошибке пишу я", "Ошиииибка", "Больше ошибок богу ошибок!!!",
+	"О Ш И Б К А", "Подумай еще", "Не то", "Не так", "Еще ошибка", "Ошибка", "Все когда-то ошибаются",
+}
+
 func (e *ErrorGenerator) GenerateErrorMessage() string {
-	return "какая-то ошибка"
+	return errTexts[rand.Int()%len(errTexts)]
 }
